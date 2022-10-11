d6a3e4bea7efea496a2d7ac662180470f9572cad
coder-sadhin <codersadhin@gmail.com>
2022-10-11 20:39:56 +0600

    header manu problem fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,161 +2,115 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [navbar, setNavbar] = useState(false);
   return (
-    <div className="bg-blue-100">
-      <div className=" px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div className="relative flex items-center justify-between">
-          <Link
-            to="/"
-            aria-label="Grand Quiz"
-            title="Grand Quiz"
-            className="inline-flex items-center"
-          >
+    <div className="bg-blue-200 px-4 py-2 mx-auto">
+      <nav className="w-full">
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+          <div>
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
+              <Link
+                to="/"
+                aria-label="Grand Quiz"
+                title="Grand Quiz"
+                className="inline-flex items-center"
+              >
 
-            <span className="ml-2 text-xl font-bold tracking-wide text-blue-800 uppercase">
-              Grand Quiz
-            </span>
-          </Link>
-          <ul className="flex items-center hidden space-x-8 lg:flex">
-            <li>
-              <Link
-                to="/home"
-                aria-label="Home"
-                title="Home"
-                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Home
+                <span className="ml-2 text-2xl font-bold tracking-wide text-blue-800 uppercase">
+                  Grand Quiz
+                </span>
               </Link>
-            </li>
-            <li>
-              <Link
-                to="/quizzes"
-                aria-label="Quizzes"
-                title="Quizzes"
-                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Quizzes
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/overview"
-                aria-label="Overview"
-                title="Overview"
-                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Overview
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/blogs"
-                aria-label="Blogs"
-                title="Blogs"
-                className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-              >
-                Blogs
-              </Link>
-            </li>
-          </ul>
-          <div className="lg:hidden">
-            <button
-              aria-label="Open Menu"
-              title="Open Menu"
-              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
-              onClick={() => setIsMenuOpen(true)}
-            >
-              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                <path
-                  fill="currentColor"
-                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-                />
-                <path
-                  fill="currentColor"
-                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-                />
-              </svg>
-            </button>
-            {isMenuOpen && (
-              <div className="absolute top-0 left-0 w-full">
-                <div className="p-5 bg-white border rounded shadow-sm">
-                  <div className="flex items-center justify-between mb-4">
-                    <div>
-                      <Link
-                        to="/"
-                        aria-label="Company"
-                        title="Company"
-                        className="inline-flex items-center"
-                      >
-
-                        <span className="ml-2 text-xl font-bold tracking-wide text-blue-800 uppercase">
-                          Grand Quiz
-                        </span>
-                      </Link>
-                    </div>
-                    <div>
-                      <button
-                        aria-label="Close Menu"
-                        title="Close Menu"
-                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                        onClick={() => setIsMenuOpen(false)}
-                      >
-                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
-                          <path
-                            fill="currentColor"
-                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
-                  </div>
-                  <nav>
-                    <ul className="space-y-4">
-                      <li>
-                        <Link
-                          to="/home"
-                          aria-label="Home"
-                          title="Home"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                        >
-                          Home
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/quizzes"
-                          aria-label="Quizzes"
-                          title="Quizzes"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                        >
-                          Quizzes
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/blogs"
-                          aria-label="Blogs"
-                          title="Blogs"
-                          className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                        >
-                          Blogs
-                        </Link>
-                      </li>
-
-                    </ul>
-                  </nav>
-                </div>
+              <div className="md:hidden">
+                <button
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                  onClick={() => setNavbar(!navbar)}
+                >
+                  {navbar ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  )}
+                </button>
               </div>
-            )}
+            </div>
+          </div>
+          <div>
+            <div
+              className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${navbar ? "block" : "hidden"
+                }`}
+            >
+              <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                <li>
+                  <Link
+                    to="/home"
+                    aria-label="Home"
+                    title="Home"
+                    className="font-bold text-xl tracking-wide text-gray-900 transition-colors duration-200 hover:text-purple-500"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/quizzes"
+                    aria-label="Quizzes"
+                    title="Quizzes"
+                    className="font-bold text-xl tracking-wide text-gray-900 transition-colors duration-200 hover:text-purple-500"
+                  >
+                    Quizzes
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/overview"
+                    aria-label="Overview"
+                    title="Overview"
+                    className="font-bold text-xl tracking-wide text-gray-900 transition-colors duration-200 hover:text-purple-500"
+                  >
+                    Overview
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/blogs"
+                    aria-label="Blogs"
+                    title="Blogs"
+                    className="font-bold text-xl tracking-wide text-gray-900 transition-colors duration-200 hover:text-purple-500"
+                  >
+                    Blogs
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
+      </nav>
     </div>
+
   );
 };
 
