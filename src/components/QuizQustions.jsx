65a7e069e57e6cbbbef135dc6584b4d3a9f4ece9
coder-sadhin <codersadhin@gmail.com>
2022-10-11 16:16:43 +0600

    eye but function worked

--- src/components/QuizQustions.jsx
+++ src/components/QuizQustions.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
+import { toast } from 'react-toastify';
 import QuizAnsware from './QuizAnsware';
+import { EyeIcon } from '@heroicons/react/24/solid'
+
 
 
 const QuizQustions = ({ quizQuestion, index, right, wrong }) => {
     const { correctAnswer, options, question } = quizQuestion;
     // console.log(question)
     const [areaDisabled, setAreaDisabled] = useState("");
+    // icone click show correct ans
+    const handleToEyeIconClick = (index, correctAnswer) => {
+        toast.info(`Quiz ${index + 1} Ans is:---- ${correctAnswer}`, { autoClose: 3000 });
+    }
 
     return (
         <div className={`border rounded shadow-md shadow-slate-300 m-3 p-5  mx-auto w-10/12 ${areaDisabled}`}>
+            <EyeIcon onClick={() => handleToEyeIconClick(index, correctAnswer)} className="absolute h-6 w-6 text-blue-500" />
             <div className='text-center mb-4 text-3xl w-6/12 mx-auto'>
                 <h3><span className='text-green-600 font-bold'>Quiz {index + 1}:</span> {question}</h3>
             </div>
